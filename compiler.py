3125b240f62700c8a5cfdd9e8356e26b3326c635
unknown <christoph02.cw@gmail.com>
2022-02-09 19:31:30 +0100

    :sparkles: create compiler

---
+++ compiler.py
@@ -0,0 +1,50 @@
+import os
+import json
+
+#load config
+configF = open("config.json")
+config = json.load(configF)
+
+#combine all markdown files
+wholeMarkdown = ""
+for kapitel in config["kapitel"]:
+    md = open("./Kapitel/"+kapitel+"/text.md").read()
+    md = md.replace("Dateien\\","Dateien-"+kapitel+"\\")
+    wholeMarkdown += md
+    #copy dateien
+    copyPDF = "xcopy Kapitel\\"+kapitel+"\\Dateien chache\\Dateien-"+kapitel+" /E/H/Y/i"
+    print(copyPDF)
+    os.system(copyPDF)
+
+
+#append header information to File
+header = open("header.md").read()
+
+header = header.replace("<author-placeholder>",config["author"])
+header = header.replace("<title-placeholder>",config["titel"])
+
+wholeMarkdown = header + "\n\n" + wholeMarkdown
+
+
+
+out = open("chache/combined.md","w")
+out.write(wholeMarkdown)
+out.close()
+
+createLatex = "pandoc chache/combined.md -f markdown -t latex -s -o chache/out.tex"
+
+os.system(createLatex)
+
+
+createPDF = "cd chache & lualatex out.tex"
+os.system(createPDF)
+
+createPDF = "cd chache & lualatex out.tex"
+os.system(createPDF)
+
+copyPDF = "copy chache\out.pdf ."
+
+os.system(copyPDF)
+
+#clear chache
+os.system("rmdir /s /q .\chache")
---
+++ header.md
@@ -0,0 +1,19 @@
+---
+author: <author-placeholder>
+title: <title-placeholder>
+geometry: margin=1in
+fontfamily: Cabin
+fontfamilyoptions: sfdefault
+numbersections: true
+toc: true
+toc-title: Inhalt
+header-includes: |
+    \usepackage{graphicx}
+    \usepackage{fancyhdr}
+    \pagestyle{fancy}
+    \setlength\headheight{20pt}
+    \rhead{Doc generator}
+---
+
+
+\newpage
